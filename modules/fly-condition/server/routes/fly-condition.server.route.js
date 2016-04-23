6113e1be6eef9ddf9dffa9c13083b1b30de301ad
otakuyu <yuquanzhou_007@hotmail.com>
2016-04-24 09:10:28 +1000

    changed api to post

--- modules/fly-condition/server/routes/fly-condition.server.route.js
+++ modules/fly-condition/server/routes/fly-condition.server.route.js
@@ -6,11 +6,15 @@ var flyCondition = require('../controllers/fly-condition.server.controller');
  */
 
 module.exports = function (app) {
-  app.route('/api/flyCondition/cluster/:lan/:lon')
-      .get(flyCondition.range);
-  app.route('/api/flyCondition/:lan/:lon')
+
+    app.route('/api/flyCondition/cluster/:lan/:lon/:name')
+      .post(flyCondition.range);
+    app.route('/api/drone/:name')
+      .get(flyCondition.isNameUnique);
     app.route('/api/flyCondition/blacklist')
         .get(flyCondition.getAllBlacklist);
-      .get(flyCondition.index);
-
+    app.route('/api/flyCondition')
+      .post(flyCondition.index);
+    app.route('/api/flyCondition/:name')
+      .post(flyCondition.index);
 };
