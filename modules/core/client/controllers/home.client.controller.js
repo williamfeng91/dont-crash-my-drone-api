8691b95068723917a2bb5e2ebc4fec6b9a11c841
William Feng <williamf@webrecs.com.au>
2016-04-24 02:25:30 +1000

    Added gauge to show risk

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -11,34 +11,56 @@
     var vm = this;
     // vm.address = "current-location";
     vm.googleMapsUrl = "https://maps.googleapis.com/maps/api/js&key=AIzaSyAqv9zZxqC-ixUCGQ_61-sgFHyptQFpRKk";
+    vm.customSectors = [
+      {
+        color: "#a9d70b",
+        lo: 0,
+        hi: 69
+      },
+      {
+        color: "#f9c802",
+        lo: 70,
+        hi: 89
+      },
+      {
+        color: "#ff0000",
+        lo: 90,
+        hi: 100
+      }
+    ];
+
     vm.search = search;
+    vm.clearAddress = clearAddress;
+    vm.showInfoWindow = function(evt) {
+      var newPos = {lat: vm.pos.lat() - 0.1, lng: vm.pos.lng()};
+      vm.map.setCenter(newPos);
+      vm.map.markers.focus.setVisible(true);
+      vm.map.showInfoWindow('info', vm.map.markers.focus);
+    };
 
     NgMap.getMap().then(function(map) {
       vm.map = map;
-      vm.showCustomMarker= function(evt) {
-        map.customMarkers.focus.setVisible(true);
-        map.customMarkers.focus.setPosition(this.getPosition());
-      };
-      vm.closeCustomMarker= function(evt) {
-        this.style.display = 'none';
-      };
     });
 
     function search() {
-      vm.place = this.getPlace();
-      vm.address = vm.place.geometry.location;
-      // console.log(vm.place);
-      vm.map.setCenter(vm.place.geometry.location);
-
-      FlyConditionService.getFlyCondition(vm.address.lat(), vm.address.lng()).then(getFlyConditionSuccess, getFlyConditionFailed);
+      vm.pos = this.getPlace().geometry.location;
+      var lat = vm.pos ? vm.pos.lat() : 0;
+      var lng = vm.pos ? vm.pos.lng() : 0;
+      FlyConditionService.getFlyCondition(lat, lng).then(getFlyConditionSuccess, getFlyConditionFailed);
 
       function getFlyConditionSuccess(result) {
         console.log(result);
+        vm.flyCondition = result[0];
+        vm.showInfoWindow();
       }
 
       function getFlyConditionFailed(error) {
         console.error(error);
       }
+    }
+
+    function clearAddress() {
+      vm.address = "";
     }
   }
 
